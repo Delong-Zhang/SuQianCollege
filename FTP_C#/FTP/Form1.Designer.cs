021159254e439b1fb4474a087bb280f318068735
Delong <Delong_Zhang_China@163.com>
2018-06-30 02:00:01 +0800

    FTP_C#_C#课程设计

---
+++ FTP_C#/FTP/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace FTP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnChangDir = new System.Windows.Forms.Button();
+            this.btnDownLoad = new System.Windows.Forms.Button();
+            this.btnConnection = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtFtpUri = new System.Windows.Forms.TextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnUp = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnChangDir
+            // 
+            this.btnChangDir.Location = new System.Drawing.Point(317, 152);
+            this.btnChangDir.Name = "btnChangDir";
+            this.btnChangDir.Size = new System.Drawing.Size(75, 23);
+            this.btnChangDir.TabIndex = 15;
+            this.btnChangDir.Text = "更换目录";
+            this.btnChangDir.UseVisualStyleBackColor = true;
+            this.btnChangDir.Click += new System.EventHandler(this.btnChangDir_Click);
+            // 
+            // btnDownLoad
+            // 
+            this.btnDownLoad.Location = new System.Drawing.Point(319, 102);
+            this.btnDownLoad.Name = "btnDownLoad";
+            this.btnDownLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnDownLoad.TabIndex = 16;
+            this.btnDownLoad.Text = "下载";
+            this.btnDownLoad.UseVisualStyleBackColor = true;
+            this.btnDownLoad.Click += new System.EventHandler(this.btnDownLoad_Click);
+            // 
+            // btnConnection
+            // 
+            this.btnConnection.Location = new System.Drawing.Point(317, 77);
+            this.btnConnection.Name = "btnConnection";
+            this.btnConnection.Size = new System.Drawing.Size(75, 23);
+            this.btnConnection.TabIndex = 17;
+            this.btnConnection.Text = "连接";
+            this.btnConnection.UseVisualStyleBackColor = true;
+            this.btnConnection.Click += new System.EventHandler(this.btnConnection_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(12, 75);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(292, 100);
+            this.listBox1.TabIndex = 14;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(349, 5);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(41, 21);
+            this.txtPort.TabIndex = 13;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(297, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "端口：";
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Location = new System.Drawing.Point(282, 41);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.PasswordChar = '*';
+            this.txtPwd.Size = new System.Drawing.Size(110, 21);
+            this.txtPwd.TabIndex = 11;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(87, 41);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(110, 21);
+            this.txtName.TabIndex = 9;
+            // 
+            // txtFtpUri
+            // 
+            this.txtFtpUri.Location = new System.Drawing.Point(118, 5);
+            this.txtFtpUri.Name = "txtFtpUri";
+            this.txtFtpUri.Size = new System.Drawing.Size(168, 21);
+            this.txtFtpUri.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(219, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "密码：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "用户名：";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 12);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "FTP服务器地址：";
+            // 
+            // btnUp
+            // 
+            this.btnUp.Location = new System.Drawing.Point(319, 127);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(75, 23);
+            this.btnUp.TabIndex = 18;
+            this.btnUp.Text = "上传";
+            this.btnUp.UseVisualStyleBackColor = true;
+            this.btnUp.Click += new System.EventHandler(this.btnUp_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 191);
+            this.Controls.Add(this.btnUp);
+            this.Controls.Add(this.btnChangDir);
+            this.Controls.Add(this.btnDownLoad);
+            this.Controls.Add(this.btnConnection);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPwd);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtFtpUri);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "EXP13_2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChangDir;
+        private System.Windows.Forms.Button btnDownLoad;
+        private System.Windows.Forms.Button btnConnection;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtFtpUri;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ FTP_C#/FTP/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace FTP
+{
+    public partial class Form1 : Form
+    {
+        string changeDir;  //更换的目录名
+        public Form1()
+        {
+            InitializeComponent();
+            txtFtpUri.Text = "ftp://delongzhang.cn";
+            txtPort.Text = "21";
+            txtName.Text = "User_20180701";
+            txtPwd.Text = "User_20180701";
+        }
+
+        private void GetFileList(Stream stream)//把FTP服务器返回的文件列表填充到ListBox控件中。
+        {
+            Encoding encodingGB2312 = System.Text.Encoding.GetEncoding("GB2312");//获取一个能够使用GB2312进行编码/解码的Encoding的对象
+
+            StreamReader strReader = new StreamReader(stream, encodingGB2312);//用指定的编码和指定的流初始化StreamReader对象
+            string content = strReader.ReadToEnd();
+            string[] files = content.Split('\n');
+            listBox1.DataSource = files;
+            strReader.Close();
+
+        }
+
+        private void btnConnection_Click(object sender, EventArgs e)
+        {
+            Cursor currentCursor = this.Cursor;//保存当前鼠标指针显示的光标
+            FtpWebResponse respones = null;
+            Stream stream = null;
+            try
+            {
+                this.Cursor = Cursors.WaitCursor;//指针显示的光标改变为等待光标
+                string ftp = txtFtpUri.Text + ":" + txtPort.Text;
+                FtpWebRequest request = (FtpWebRequest)WebRequest.Create(ftp);//创建WebRequest实例
+                request.Credentials = new NetworkCredential(txtName.Text, txtPwd.Text);//设置与FTP服务器通信的凭证
+                request.Method = WebRequestMethods.Ftp.ListDirectory;//设置要发送到FTP服务器的命令是获取文件列表
+                respones = (FtpWebResponse)request.GetResponse();//返回服务器的响应
+                stream = respones.GetResponseStream();//获取包含从FTP服务器上发送的响应数据的流
+                GetFileList(stream);////把FTP服务器返回的文件列表填充到ListBox控件中
+                changeDir = string.Empty; ;
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (respones != null) respones.Close();
+                if (stream != null) stream.Close();
+                this.Cursor = currentCursor;//恢复鼠标指针显示的光标
+
+            }
+        }
+
+        private void btnDownLoad_Click(object sender, EventArgs e)
+        {
+            Cursor currentCursor = this.Cursor;//保存当前鼠标指针显示的光标
+            FtpWebResponse respones = null;
+            Stream inStream = null;
+            Stream outStream = null;
+
+            try
+            {
+                this.Cursor = Cursors.WaitCursor;//指针显示的光标改变为等待光标
+                string ftp = txtFtpUri.Text + ":" + txtPort.Text;
+                Uri baseUri = new Uri(ftp);
+                string fileName = listBox1.SelectedItem.ToString().Trim(); ;
+                string fullName = changeDir + "/" + fileName;
+                Uri uri = new Uri(baseUri, fullName);
+
+                FtpWebRequest request = (FtpWebRequest)WebRequest.Create(uri);//创建WebRequest实例
+                request.Credentials = new NetworkCredential(txtName.Text, txtPwd.Text);//设置与FTP服务器通信的凭证
+                request.Method = WebRequestMethods.Ftp.DownloadFile;//设置要发送到FTP服务器的命令是下载文件
+                respones = (FtpWebResponse)request.GetResponse();//返回服务器的响应
+                inStream = respones.GetResponseStream();//获取包含从FTP服务器上发送的响应数据的流
+                saveFileDialog1.FileName = fileName;
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    outStream = File.OpenWrite(saveFileDialog1.FileName);//打开文件以便写入文件
+                    byte[] buffer = new byte[1024];
+                    int size = 0;
+                    while ((size = inStream.Read(buffer, 0, 1024)) > 0) //每次读取1024个字节并写入
+                    {
+                        outStream.Write(buffer, 0, size);
+                    }
+                    MessageBox.Show(String.Format("文件{0}\n下载成功！", saveFileDialog1.FileName), "成功", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (respones != null) respones.Close();
+                if (inStream != null) inStream.Close();
+                if (outStream != null) outStream.Close();
+                this.Cursor = currentCursor;
+
+            }
+        }
+
+        private void btnUp_Click(object sender, EventArgs e)
+        {
+            Cursor currentCursor = this.Cursor;//保存当前鼠标指针显示的光标
+            FtpWebResponse respones = null;
+            Stream inStream = null;
+            Stream outStream = null;
+
+            try
+            {
+
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    this.Cursor = Cursors.WaitCursor;//指针显示的光标改变为等待光标
+                    string ftp = txtFtpUri.Text + ":" + txtPort.Text;
+                    Uri baseUri = new Uri(ftp);
+                    string fileName = openFileDialog1.FileName;
+                    string fullName = changeDir + "/" + fileName.Substring(fileName.LastIndexOf('\\') + 1);
+                    Uri uri = new Uri(baseUri, fullName);
+
+                    FtpWebRequest request = (FtpWebRequest)WebRequest.Create(uri);//创建WebRequest实例
+                    request.Credentials = new NetworkCredential(txtName.Text, txtPwd.Text);//设置与FTP服务器通信的凭证
+                    request.Method = WebRequestMethods.Ftp.UploadFile;//设置要发送到FTP服务器的命令是下载文件
+                    outStream = request.GetRequestStream();
+                    //respones = (FtpWebResponse)request.GetResponse();//返回服务器的响应
+                    //outStream = respones.GetResponseStream();//获取包含从FTP服务器上发送的响应数据的流
+                    inStream = File.OpenRead(openFileDialog1.FileName);//打开文件以便写入文件
+                    byte[] buffer = new byte[1024];
+                    int size = 0;
+                    while ((size = inStream.Read(buffer, 0, 1024)) > 0) //每次读取1024个字节并写入
+                    {
+                        outStream.Write(buffer, 0, size);
+                    }
+                    MessageBox.Show(String.Format("文件{0}\n上传成功！", fileName), "成功", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (respones != null) respones.Close();
+                if (inStream != null) inStream.Close();
+                if (outStream != null) outStream.Close();
+                this.Cursor = currentCursor;
+
+            }
+        }
+
+        private void btnChangDir_Click(object sender, EventArgs e)
+        {
+            Cursor currentCursor = this.Cursor;
+            FtpWebResponse respones = null;
+            Stream stream = null;
+            try
+            {
+                this.Cursor = Cursors.WaitCursor;
+                string subDir = listBox1.SelectedItem.ToString().Trim();
+                changeDir += "/" + subDir;
+                string ftp = txtFtpUri.Text + ":" + txtPort.Text;
+                Uri baseUri = new Uri(ftp);
+                Uri uri = new Uri(baseUri, changeDir);
+
+                FtpWebRequest request = (FtpWebRequest)WebRequest.Create(uri);
+                request.Credentials = new NetworkCredential(txtName.Text, txtPwd.Text);
+                request.Method = WebRequestMethods.Ftp.ListDirectory;
+                respones = (FtpWebResponse)request.GetResponse();
+                stream = respones.GetResponseStream();
+                GetFileList(stream);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (respones != null) respones.Close();
+                if (stream != null) stream.Close();
+                this.Cursor = currentCursor;
+
+            }
+        }
+    }
+}
